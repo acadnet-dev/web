e79c1c1714faf7c16b71329d1d2a5773dde834ab
Dimitrie David <david.dimitrie@gmail.com>
2023-06-05 01:06:53 +0300

    removed throwing caught exceptions

--- src/Framework/Services/FileServices/MinioFileService.cs
+++ src/Framework/Services/FileServices/MinioFileService.cs
@@ -17,228 +17,144 @@ namespace Framework.Services.FileServices
 
         public async Task CreateBucketAsync(string bucketName)
         {
-            try
-            {
-                var client = _minioService.GetClient();
+            var client = _minioService.GetClient();
 
-                // Make a bucket on the server, if not already present
-                var beArgs = new Minio.BucketExistsArgs()
+            // Make a bucket on the server, if not already present
+            var beArgs = new Minio.BucketExistsArgs()
+                .WithBucket(bucketName);
+
+            bool found = client.BucketExistsAsync(beArgs).Result;
+            if (!found)
+            {
+                var mbArgs = new Minio.MakeBucketArgs()
                     .WithBucket(bucketName);
-
-                bool found = client.BucketExistsAsync(beArgs).Result;
-                if (!found)
-                {
-                    var mbArgs = new Minio.MakeBucketArgs()
-                        .WithBucket(bucketName);
-                    await client.MakeBucketAsync(mbArgs);
-                }
-            }
-            catch (Exception ex)
-            {
-                throw ex;
+                await client.MakeBucketAsync(mbArgs);
             }
         }
 
         public async Task DeleteFileAsync(string bucketName, string fileName)
         {
-            try
+            // check if bucket exists and if file exists
+            var beArgs = new Minio.BucketExistsArgs()
+                .WithBucket(bucketName);
+
+            if (!await _minioService.GetClient().BucketExistsAsync(beArgs))
             {
-                // check if bucket exists and if file exists
-                var beArgs = new Minio.BucketExistsArgs()
-                    .WithBucket(bucketName);
-
-                if (!await _minioService.GetClient().BucketExistsAsync(beArgs))
-                {
-                    return;
-                }
-
-                var soArgs = new Minio.StatObjectArgs()
-                    .WithBucket(bucketName)
-                    .WithObject(fileName);
-
-                if (await _minioService.GetClient().StatObjectAsync(soArgs) == null)
-                {
-                    return;
-                }
-
-                var client = _minioService.GetClient();
-
-                var removeObjectArgs = new Minio.RemoveObjectArgs()
-                    .WithBucket(bucketName)
-                    .WithObject(fileName);
-
-                await client.RemoveObjectAsync(removeObjectArgs);
+                return;
             }
-            catch (Exception ex)
+
+            var soArgs = new Minio.StatObjectArgs()
+                .WithBucket(bucketName)
+                .WithObject(fileName);
+
+            if (await _minioService.GetClient().StatObjectAsync(soArgs) == null)
             {
-                throw ex;
+                return;
             }
+
+            var client = _minioService.GetClient();
+
+            var removeObjectArgs = new Minio.RemoveObjectArgs()
+                .WithBucket(bucketName)
+                .WithObject(fileName);
+
+            await client.RemoveObjectAsync(removeObjectArgs);
         }
 
         public async Task<S3Object?> DownloadFileAsync(string bucketName, string fileName)
         {
-            try
+            // sync
+            AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+
+            var client = _minioService.GetClient();
+
+            // get stat object
+            var soArgs = new Minio.StatObjectArgs()
+                .WithBucket(bucketName)
+                .WithObject(fileName);
+
+            var statObject = await client.StatObjectAsync(soArgs).ConfigureAwait(false);
+
+            if (statObject == null)
             {
-                // sync
-                AutoResetEvent autoResetEvent = new AutoResetEvent(false);
-
-                var client = _minioService.GetClient();
-
-                // get stat object
-                var soArgs = new Minio.StatObjectArgs()
-                    .WithBucket(bucketName)
-                    .WithObject(fileName);
-
-                var statObject = await client.StatObjectAsync(soArgs).ConfigureAwait(false);
-
-                if (statObject == null)
-                {
-                    return null;
-                }
-
-                S3Object s3Object = new S3Object()
-                {
-                    BucketName = bucketName,
-                    FileName = fileName,
-                    ContentType = statObject.ContentType,
-                    Content = new MemoryStream()
-                };
-
-                var getObjectArgs = new Minio.GetObjectArgs()
-                    .WithBucket(bucketName)
-                    .WithObject(fileName)
-                    .WithCallbackStream((stream) =>
-                    {
-                        stream.CopyTo(s3Object.Content);
-                        // reset stream position
-                        s3Object.Content.Position = 0;
-                        autoResetEvent.Set();
-                    });
-
-                var stream = await client.GetObjectAsync(getObjectArgs).ConfigureAwait(false);
-
-                autoResetEvent.WaitOne();
-
-                return s3Object;
+                return null;
             }
-            catch (Exception ex)
+
+            S3Object s3Object = new S3Object()
             {
-                throw ex;
-            }
+                BucketName = bucketName,
+                FileName = fileName,
+                ContentType = statObject.ContentType,
+                Content = new MemoryStream()
+            };
+
+            var getObjectArgs = new Minio.GetObjectArgs()
+                .WithBucket(bucketName)
+                .WithObject(fileName)
+                .WithCallbackStream((stream) =>
+                {
+                    stream.CopyTo(s3Object.Content);
+                    // reset stream position
+                    s3Object.Content.Position = 0;
+                    autoResetEvent.Set();
+                });
+
+            var stream = await client.GetObjectAsync(getObjectArgs).ConfigureAwait(false);
+
+            autoResetEvent.WaitOne();
+
+            return s3Object;
         }
 
         public ICollection<S3ObjectStat> GetFilesInBucket(string bucketName)
         {
-            try
-            {
-                // sync
-                AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+            // sync
+            AutoResetEvent autoResetEvent = new AutoResetEvent(false);
 
-                var client = _minioService.GetClient();
+            var client = _minioService.GetClient();
 
-                var listObjectsArgs = new Minio.ListObjectsArgs()
-                    .WithBucket(bucketName);
+            var listObjectsArgs = new Minio.ListObjectsArgs()
+                .WithBucket(bucketName);
 
-                var observable = client.ListObjectsAsync(listObjectsArgs);
+            var observable = client.ListObjectsAsync(listObjectsArgs);
 
-                List<S3ObjectStat> objects = new List<S3ObjectStat>();
+            List<S3ObjectStat> objects = new List<S3ObjectStat>();
 
-                IDisposable? subscription = observable.Subscribe(
-                    item => objects.Add(new S3ObjectStat { BucketName = bucketName, FileName = item.Key }),
-                    ex => throw ex,
-                    () => autoResetEvent.Set());
+            IDisposable? subscription = observable.Subscribe(
+                item => objects.Add(new S3ObjectStat { BucketName = bucketName, FileName = item.Key }),
+                ex => throw ex,
+                () => autoResetEvent.Set());
 
-                autoResetEvent.WaitOne();
+            autoResetEvent.WaitOne();
 
-                return objects;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+            return objects;
         }
 
         public async Task UploadFileAsync(S3Object s3object)
         {
-            var checkStructureREsult = CheckFileStructure(s3object);
-            if (checkStructureREsult != null)
-            {
-                throw new Exception(checkStructureREsult);
-            }
+            var client = _minioService.GetClient();
 
-            try
-            {
-                var client = _minioService.GetClient();
+            // Make a bucket on the server, if not already present
+            var beArgs = new Minio.BucketExistsArgs()
+                .WithBucket(s3object.BucketName);
 
-                // Make a bucket on the server, if not already present
-                var beArgs = new Minio.BucketExistsArgs()
+            bool found = await client.BucketExistsAsync(beArgs).ConfigureAwait(false);
+            if (!found)
+            {
+                var mbArgs = new Minio.MakeBucketArgs()
                     .WithBucket(s3object.BucketName);
-
-                bool found = await client.BucketExistsAsync(beArgs).ConfigureAwait(false);
-                if (!found)
-                {
-                    var mbArgs = new Minio.MakeBucketArgs()
-                        .WithBucket(s3object.BucketName);
-                    await client.MakeBucketAsync(mbArgs).ConfigureAwait(false);
-                }
-
-                // Upload a file to bucket
-                var putObjectArgs = new Minio.PutObjectArgs()
-                    .WithBucket(s3object.BucketName)
-                    .WithObject(s3object.FileName)
-                    .WithObjectSize(s3object.Content.Length)
-                    .WithContentType(s3object.ContentType)
-                    .WithStreamData(s3object.Content);
-
-                await client.PutObjectAsync(putObjectArgs).ConfigureAwait(false);
-
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        // File validation
-        private string? CheckFileStructure(S3Object s3object)
-        {
-            /// Problem structure
-            ///
-            /// problem/
-            /// ├─ README.md            * problem statement
-            /// ├─ main.cpp             * main problem file
-            /// ├─ solution_main.cpp    * main problem solution
-            /// ├─ test0.in             * test input
-            /// ├─ test1.in
-            /// │  ...
-            /// ├─ test0.ref            * test reference
-            /// ├─ test1.ref
-            /// │  ...
-            /// 
-
-            // check if file is in the structure
-
-            // README.md
-            if (s3object.FileName == "README.md")
-            {
-                return null;
+                await client.MakeBucketAsync(mbArgs).ConfigureAwait(false);
             }
 
-            // main and sol cpp / c
-            if (s3object.FileName == "main.cpp" ||
-                s3object.FileName == "solution_main.cpp")
-            {
-                return null;
-            }
+            // Upload a file to bucket
+            var putObjectArgs = new Minio.PutObjectArgs()
+                .WithBucket(s3object.BucketName)
+                .WithObject(s3object.FileName)
+                .WithObjectSize(s3object.Content.Length)
+                .WithContentType(s3object.ContentType)
+                .WithStreamData(s3object.Content);
 
-            // tests
-            if (s3object.FileName.StartsWith("test") &&
-                (s3object.FileName.EndsWith(".in") || s3object.FileName.EndsWith(".ref")))
-            {
-                return null;
-            }
-
-            return "File is not in the defined structure";
+            await client.PutObjectAsync(putObjectArgs).ConfigureAwait(false);
         }
     }
 }
